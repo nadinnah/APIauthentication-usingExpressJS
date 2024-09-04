ff8f1a64ef580628f396efc3f16ff5cded6cf9a1
nadinnah <nadinahmedmohamed@gmail.com>
2024-09-04 11:39:16 +0300

    last version2

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com/";
 
-const yourUsername = "nadinnah";
-const yourPassword = "nadinnah";
-const yourAPIKey = "ad83eb06-c5b8-421a-90ff-8e42d6899bd2";
-const yourBearerToken = "9ba4809e-9791-4df5-865a-1e3fb9779611";
+const yourUsername = "";
+const yourPassword = "";
+const yourAPIKey = "";
+const yourBearerToken = "";
 
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "API Response." });
